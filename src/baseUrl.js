720963c0cbb555e44ffb5202dfd4911c528a2a31
COLMAN <127256819+COLMANRL@users.noreply.github.com>
2025-07-20 03:44:22 -0700

    Update baseUrl.js

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = "https://gakenya-f4aed3ddefakhqfx.southafricanorth-01.azurewebsites.net";
+const baseUrl = "https://gakenya-f4aed3ddefakhqfx.eastus-01.azurewebsites.net/";
 export default baseUrl;
